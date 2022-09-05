9d09b1a91365d75c9750a5391daae23800823d92
unknown <ansuzpert88@gmail.com>
2022-09-05 19:46:31 +0300

    Все задания

---
+++ 01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("ВВедите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+string func (int per1, int per2)
+{
+    int z = per1;
+while (per2!=1)
+{
+    per2 = per2-1;
+    z = z*per1;
+}
+return($"{per1} в заданной степени равна {z}.");
+}
+Console.WriteLine(func(x,y));
---
+++ 02/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+int func (int per1)
+{
+int sum=0;
+while (x>=1)
+{
+    sum = sum + (x%10);
+    x = x/10;
+}
+return(sum);
+}
+Console.WriteLine(func(x));
